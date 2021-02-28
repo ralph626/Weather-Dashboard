9903b7671aa0fa16a6c5163fb14ca1f5fb4a527a
Rafael_O <ralph.orozco@gmail.com>
2021-02-27 20:42:27 -0800

    added some commets and missing closing tags

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const APPKEY = "166a433c57516f51dfab1f7edaed8413";
 {"coord":{"lon":-87.65,"lat":41.85},"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"base":"stations","main":{"temp":47.62,"feels_like":40.28,"temp_min":44.6,"temp_max":50,"pressure":1018,"humidity":66},"visibility":10000,"wind":{"speed":8.05,"deg":180},"clouds":{"all":40},"dt":1614471056,"sys":{"type":1,"id":4861,"country":"US","sunrise":1614428908,"sunset":1614469105},"timezone":-21600,"id":4887398,"name":"Chicago","cod":200}
 */
 var HISTORY = [];
-
+//this gets the current day weather
 function searchWeather() {
   const searchValue = $("#search-value").val();
   HISTORY.push(searchValue);
@@ -30,6 +30,7 @@ function searchWeather() {
     },
   });
 }
+//this gets the weather for the next 5 days
 function getForecast() {
   const searchValue = $("#search-value").val();
   var url =
@@ -38,13 +39,14 @@ function getForecast() {
     "&appid=" +
     APPKEY +
     "&units=imperial";
+
   $.get({
     type: "GET",
     url: url,
     dataType: "json",
     success: function (data) {
       console.log(data);
-
+      //this is for forecast. it was showing every 3 hours increments so I added 40 then divided the i by 8.
       for (var i = 0; i < 40; i += 8) {
         console.log(i);
         const dt = data.list[i].dt_txt;
@@ -53,23 +55,16 @@ function getForecast() {
         const html = `Date: ${dt}<br/>Temp: ${temp}<br/>Humidity: ${hum}<hr/>`;
         const card = `
         <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
-  <div class="card-header">Date: ${dt}</div>
-  <div class="card-body">
-    <h4 class="card-title">Temp: ${temp}</h4>
-    <p class="card-text">Humidity: ${hum}</p>
-  </div>
-</div>
+          <div class="card-header">Date: ${dt}</div>
+            <div class="card-body">
+              <h4 class="card-title">Temp: ${temp}</h4>
+              <p class="card-text">Humidity: ${hum}</p>
+            </div>
+        </div>
         `;
-        // alert(html);
+
         $("#day" + (i / 8 + 1)).html(card);
       }
-      /*
-          $("#humidity").text(data.main.humidity);
-          $("#temperature").text(data.main.temp);
-          $("#windSpeed").text(data.wind.speed);
-          */
-      //  alert(data.main.humidity);
-      // create history link for this search
     },
   });
 }
